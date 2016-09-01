980f77df20cff9ad2f487aaa510f907b1e46458b
Travis Shepherd <travisshepherd@outlook.com>
2016-08-31 23:29:46 -0500

    update gulp and bundling

--- src/Angular2onNETCore/gulpfile.js
+++ src/Angular2onNETCore/gulpfile.js
@@ -7,7 +7,7 @@ gulp.task('default', function () {
 var paths = {};
 paths.webroot = "wwwroot/";
 paths.npmSource = "node_modules/";
-paths.npmLib = paths.webroot + "lib_npm/";
+paths.npmLib = paths.webroot + "lib/";
 
 gulp.task("copy-deps:systemjs", function () {
     return gulp.src(paths.npmSource + 'systemjs/dist/**/*.js').pipe(gulp.dest(paths.npmLib + 'systemjs'));
@@ -22,7 +22,23 @@ gulp.task("copy-deps:rxjs", function () {
 });
 
 gulp.task("copy-deps:angular2-in-memory-web-api", function () {
-    return gulp.src(paths.npmSource + 'angular2-in-memory-web-api/*.js').pipe(gulp.dest(paths.npmLib + 'angular2-in-memory-web-api'));
+    return gulp.src(paths.npmSource + 'angular2-in-memory-web-api/**/*.js').pipe(gulp.dest(paths.npmLib + 'angular2-in-memory-web-api'));
 });
 
-gulp.task("copy-deps", ["copy-deps:rxjs", 'copy-deps:angular2', 'copy-deps:systemjs', 'copy-deps:angular2-in-memory-web-api']);
+gulp.task("copy-deps:core-js", function () {
+    return gulp.src(paths.npmSource + 'core-js/client/*.js').pipe(gulp.dest(paths.npmLib + 'core-js'));
+});
+
+gulp.task("copy-deps:reflect-metadata", function () {
+    return gulp.src(paths.npmSource + 'reflect-metadata/*.js').pipe(gulp.dest(paths.npmLib + 'reflect-metadata'));
+});
+
+gulp.task("copy-deps:zone.js", function () {
+    return gulp.src(paths.npmSource + 'zone.js/dist/*.js').pipe(gulp.dest(paths.npmLib + 'zone.js'));
+});
+
+gulp.task("copy-deps:systemjs.config", function () {
+    return gulp.src('systemjs.config.js').pipe(gulp.dest(paths.webroot + 'js'));
+});
+
+gulp.task("copy-all-deps", ["copy-deps:rxjs", 'copy-deps:angular2', 'copy-deps:systemjs', 'copy-deps:angular2-in-memory-web-api', 'copy-deps:core-js', 'copy-deps:reflect-metadata', 'copy-deps:zone.js']);
--- src/Angular2onNETCore/wwwroot/js/systemjs.config.js
+++ src/Angular2onNETCore/wwwroot/js/systemjs.config.js
@@ -6,9 +6,9 @@
     // map tells the System loader where to look for things
     var map = {
         'app': 'js', // 'dist',
-        '@angular': 'lib_npm/angular',
-        'angular2-in-memory-web-api': 'lib_npm/angular2-in-memory-web-api',
-        'rxjs': 'lib_npm/rxjs'
+        '@angular': 'lib/angular',
+        'angular2-in-memory-web-api': 'lib/angular2-in-memory-web-api',
+        'rxjs': 'lib/rxjs'
     };
     // packages tells the System loader how to load when no filename and/or no extension
     var packages = {
